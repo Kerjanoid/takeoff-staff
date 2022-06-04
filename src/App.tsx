d2b551e3e880ac18003c78a6d9363985f2f3e5f6
Vyacheslav Bardakov <99280583+VyacheslavB13@users.noreply.github.com>
2022-06-04 23:33:12 +0300

    Fix Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Contacts from './components/Contacts/Contacts';
 import NotFound from './components/NotFound/NotFound';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import SignIn from './components/SignIn/SignIn';
 
 const App: React.FC = () => {
-  const auth: boolean = true
 
   return (
     <Routes>
-      <Route path='/sign-in' element={<SignIn auth={auth} />} />
-
-      <Route element={<ProtectedRoute auth={auth} />}>
-        <Route path='/contacts' element={<Contacts />} />
-      </Route>
-
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/' element={<Contacts />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
   );
