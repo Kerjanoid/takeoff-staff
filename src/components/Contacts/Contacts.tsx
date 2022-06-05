5081c2ceb2a6c65152640621c8439f85289a198e
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2022-06-05 21:04:33 +0300

    Fix code style

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,31 +1,32 @@
-import { Container, Box } from '@mui/material';
-import { Navigate } from 'react-router-dom';
-import { useTypedSelector } from '../../hooks/useTypedSelector';
-import CardList from '../CardList/CardList';
-import Header from '../Header/Header';
+import { Container, Box } from "@mui/material";
+import { Navigate } from "react-router-dom";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import CardList from "../CardList/CardList";
+import Header from "../Header/Header";
 
 const Contacts: React.FC = () => {
-  const { isLoggedIn } = useTypedSelector(state => state.auth)
+  const { isLoggedIn } = useTypedSelector((state) => state.auth);
 
   if (!isLoggedIn) {
-    return <Navigate to='/sign-in' replace />;
+    return <Navigate to="/sign-in" replace />;
   }
 
   return (
-    <Container
-      maxWidth='md'
-      sx={{ my: '20px' }} >
-      <Box sx={{
-        border: '2px solid rgba(61, 92, 145, .2)',
-        borderRadius: '5px',
-        bgcolor: 'rgba(184, 229, 255, .1)',
-        minHeight: '400px',
-        overflow: 'hidden' }}>
+    <Container maxWidth="md" sx={{ my: "20px" }}>
+      <Box
+        sx={{
+          border: "2px solid rgba(61, 92, 145, .2)",
+          borderRadius: "5px",
+          bgcolor: "rgba(184, 229, 255, .1)",
+          minHeight: "400px",
+          overflow: "hidden",
+        }}
+      >
         <Header />
         <CardList />
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default Contacts;
